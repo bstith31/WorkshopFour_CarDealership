95fbec4f259d970a9aba4914687fef4e0e9761ff
Brian <brian.stith0331@gmail.com>
2024-05-10 11:55:52 -0400

    phase 1 complete

---
+++ src/main/java/com/ps/Dealership.java
@@ -0,0 +1,90 @@
+package com.ps;
+
+import java.util.ArrayList;
+
+public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList <Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Dealership{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", inventory=" + inventory +
+                '}';
+    }
+
+    //addVehicle
+    public ArrayList<Vehicle> addVehicle() {
+//        this.inventory.add();
+        return null;
+    }
+
+
+    //getAllVehicle
+    public ArrayList<Vehicle> getAllVehicle() {
+        return this.inventory;
+    }
+
+    //removeVehicle
+    public ArrayList<Vehicle> removeVehicle() {
+//          this.inventory.remove();
+          return null;
+       }
+
+    //Search Vehicles by categories
+    public ArrayList<Vehicle> getVehicleByPrice(int price){
+        return null;
+    }
+    public ArrayList<Vehicle> getVehicleByMakeModel(String make, String model){
+        return null;
+    }
+    public ArrayList<Vehicle> getVehicleByYear(int year){
+        return null;
+    }
+    public ArrayList<Vehicle> getVehicleByColor(String color){
+        return null;
+    }
+    public ArrayList<Vehicle> getVehicleByMileage(int mileage){
+        return null;
+    }
+    public ArrayList<Vehicle> getVehicleByType(String vehicleType){
+        return null;
+    }
+}
